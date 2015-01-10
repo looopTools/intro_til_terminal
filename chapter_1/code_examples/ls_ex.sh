3307601bf7d3caa4fac4b2c55750cb22151be711
Lars Nielsen <lnc13.lars@gmail.com>
2015-01-10 23:06:58 +0100

    start introduktion

---
+++ chapter_1/code_examples/ls_ex.sh
@@ -0,0 +1,14 @@
+MacBook-Pro:intro_til_terminal tools$ ls
+book.aux
+book.log
+book.out
+book.tex
+book.toc
+chapter_1
+compile.sh
+formalia
+lst_settings.aux
+lst_settings.tex
+preamble.aux
+preamble.tex
+preamble.tex.aux
